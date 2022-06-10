da702a75ed01e2b7de56fe6cd484a59b5f90b91d
Cizika <luciziks@hotmail.com>
2022-06-10 19:00:23 -0300

    Mudança main

--- main.c
+++ main.c
@@ -2,21 +2,19 @@
 #include <stdio.h>
 #include "leitura.h"
 
-// TODO: Redigir Relatório (1)
 // TODO: Críticas e Propostas de Melhorias (12)
 // TODO: Descrição da experiência do Projeto (13)
 
-//genoma grande.txt, pos genes grande.csv, fragmentos pequeno.txt)
 int main(void)
 {
-    FILE *genoma = fopen("./Entrada/genoma_grande.txt", "r");
-    FILE *pos_genes = fopen("./Entrada/pos_genes_grande.csv", "r");
-    FILE *fragmentos = fopen("./Entrada/fragmentos_grande.txt", "r");
+    FILE *genoma = fopen("./Entrada/genoma_pequeno.txt", "r");
+    FILE *pos_genes = fopen("./Entrada/pos_genes_pequeno.csv", "r");
+    FILE *fragmentos = fopen("./Entrada/fragmentos_pequeno.txt", "r");
 
     FILE *atividade_genica = fopen("./Saida/atividade_genica.txt", "w");
     FILE *pos_fragmentos = fopen("./Saida/pos_fragmentos.csv", "w+");
 
-    contagem_leituras(genoma, pos_genes, fragmentos, pos_fragmentos, 10000, 30000, atividade_genica);
+    contagem_leituras(genoma, pos_genes, fragmentos, pos_fragmentos, 100, 300, atividade_genica);
 
     fclose(atividade_genica);
     fclose(pos_fragmentos);
